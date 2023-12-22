08b87fdea86fe62f75b016a0a9ebf1c978fffc0a
keluokeda <913918146@qq.com>
2023-12-22 09:32:39 +0800

    Initial commit

---
+++ src/main/java/com/ke/jsbridge/JsBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.ke.jsbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ke/jsbridge/JsBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ke.jsbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
